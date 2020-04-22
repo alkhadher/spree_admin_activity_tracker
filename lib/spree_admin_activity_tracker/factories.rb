ed43cd1ea8b86d70dcbc420ac1acc104e12f90c9
Khadher Alqiari <k.alqiari@floranow.com>
2020-04-22 22:35:31 +0300

    Add admin tracking

---
+++ lib/spree_admin_activity_tracker/factories.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
+  #
+  # Example adding this to your spec_helper will load these Factories for use:
+
+  # require 'spree_admin_activity_tracker/factories'
+  factory :tracking, class: Spree::Tracking do
+    association :user, factory: :user
+  end
+end
